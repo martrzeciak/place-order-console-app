b89124b6cf445ada1cbda0004f38aa8728f0da97
martrzeciak <mariusz.trzeciak09@gmail.com>
2024-12-11 02:00:01 +0100

    Add Product model

---
+++ Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace PlaceOrderConsoleApp.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = default!;
+    public decimal Price { get; set; }
+}
